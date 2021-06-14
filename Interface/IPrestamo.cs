a3ee6d67de8a65be211a498e89229a1bdcf486bd
Oscar <oscargsc16u@gmail.com>
2021-06-14 13:48:35 -0600

    Interface IPrestamo

---
+++ Interface/IPrestamo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditoBancario.Interface
+{
+    interface IPrestamo
+    {
+        List<Gasto> Gastos { get; set; }
+        Moneda Moneda { get; set; }
+        decimal Monto { get; set; }
+        string Nombre { get; set; }
+        int PlazoMeses { get; set; }
+
+        public float ObtenerPorcentajeTasaVariable(Type tipoBanco)
+        {
+            return 0;
+        }
+    }
+}
