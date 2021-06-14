2a036d28c8f16949eb2b0074471a5ee5410173c7
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2021-06-14 13:41:55 -0600

    ciertas clases

---
+++ Factorys/BancoFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditoBancario.Factorys
+{
+    class BancoFactory
+    {
+        public IBanco CrearBanco(bool bn, bool bcr, bool popular, Cliente cliente, IPrestamo prestamo)
+        {
+
+        }
+    }
+}
