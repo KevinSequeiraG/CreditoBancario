bbad18d16cc4ed01d8fe21d0302c67993b3f5082
Oscar <oscargsc16u@gmail.com>
2021-06-14 13:42:12 -0600

    Interface IBANCO

---
+++ Interface/IBanco.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditoBancario.Interface
+{
+    interface IBanco
+    {
+        Cliente Cliente { get; set; }
+        List<Cuota> Cuotas { get; set; }
+        string Nombre { get; }
+        float PorcentajePrima { get; }
+        IPrestamo Prestamo { get; set; }
+
+        public void CalcularCuotas()
+        {
+
+        }
+
+        public decimal CalcularIngresoMinimo()
+        {
+            return 0;
+        }
+
+        public decimal CalcularOtrosGastos
+        {
+            return 0;
+        }
+    }
+}
