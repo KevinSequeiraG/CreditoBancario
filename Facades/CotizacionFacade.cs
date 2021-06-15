98da784be7ef065c9013157e9f51cc5ff473c239
Oscar <oscargsc16u@gmail.com>
2021-06-14 20:16:55 -0600

    cotizacion facade

--- Facades/CotizacionFacade.cs
+++ Facades/CotizacionFacade.cs
@@ -1,9 +1,12 @@
-﻿using CreditoBancario.Interface;
+﻿using CreditoBancario.Clases;
+using CreditoBancario.Interface;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace CreditoBancario.Facades
 {
@@ -13,6 +16,82 @@ namespace CreditoBancario.Facades
 
         public void GuardarXml(string ruta)
         {
+            
+            XmlDocument doc = new XmlDocument();
+
+            if (File.Exists(ruta) == false)
+            {
+                doc.LoadXml("<Prestamo></Prestamo>");
+                XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", null, null);
+                doc.AppendChild(dec);
+
+                string rutaXslt = System.Windows.Forms.Application.StartupPath + "\\Xslt\\CreditoBancario.xslt";
+                XmlProcessingInstruction xslt = doc.CreateProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"" + rutaXslt + "\"");
+                doc.AppendChild(xslt);
+            }
+            else
+            {
+                doc.Load(ruta);
+            }
+
+            XmlElement root = doc.DocumentElement;
+
+            XmlElement Banco = doc.CreateElement("Banco");
+            Banco.SetAttribute("Nombre", banco.Nombre);
+            root.AppendChild(Banco);
+
+            XmlElement Informacion = doc.CreateElement("Informacion");
+            Informacion.SetAttribute("Tipo", banco.GetType().ToString());
+            Informacion.SetAttribute("Plazo", banco.Prestamo.PlazoMeses.ToString());
+            Informacion.SetAttribute("Moneda", banco.Prestamo.Moneda.ToString());
+            root.AppendChild(Informacion);
+            
+            //// Hijos de informacion
+            XmlElement nodoMonto = doc.CreateElement("Monto");
+            nodoMonto.InnerText = banco.Prestamo.Monto.ToString();
+            Informacion.AppendChild(nodoMonto);
+
+            XmlElement nodoPrima = doc.CreateElement("Prima");
+            // duda de prima
+            nodoPrima.InnerText = banco.PorcentajePrima.ToString();
+            Informacion.AppendChild(nodoPrima);
+            ////
+
+            XmlElement Cliente = doc.CreateElement("Cliente");
+            Informacion.SetAttribute("Identificacion", banco.Cliente.identidicacion);
+            Informacion.SetAttribute("Nombre", banco.Cliente.nombre);
+            Informacion.SetAttribute("Telefono", banco.Cliente.telefono);
+            root.AppendChild(Cliente);
+
+            ////Hijos de Cliente
+            XmlElement nodoIngresoMinimo = doc.CreateElement("IngresoMinimo");
+            nodoIngresoMinimo.InnerText = banco.CalcularIngresoMinimo().ToString();
+            Cliente.AppendChild(nodoIngresoMinimo);
+            ////
+
+            XmlElement Gastos = doc.CreateElement("Gastos");
+            root.AppendChild(Gastos);
+
+            ////Hijos de Gastos
+            foreach (Gasto item in banco.Prestamo.Gastos)
+            {
+                XmlElement nodoOtrosGastos = doc.CreateElement("OtroGasto");
+                nodoOtrosGastos.SetAttribute("Monto", item.Monto.ToString());
+                if (item is SeguroVida)
+                {
+                    nodoOtrosGastos.InnerText = banco.Prestamo.Gastos.ToString();
+                }
+                else if (item is SeguroDesempleo)
+                {
+
+                }else if(item is Avaluo)
+                {
+
+                }
+                
+            }
+            ////
+
 
         }
 
